df27b542a994cbe7d910c9dba6e509f9180f8089
771salameche <salah.k2y07@gmail.com>
2026-02-28 15:21:26 +0000

    feat: Initialize the Spring Boot backend application with main and test entry points.

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.store;
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StoreApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
